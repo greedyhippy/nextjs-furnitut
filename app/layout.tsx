71a9005c2741f3cce4711ad21dd54d812ab0d61a
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-05-06 15:16:19 +0300

    no meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,42 +9,6 @@ import { FrontPageMetadataDocument } from '@/generated/graphql';
 
 const manrope = Manrope({ subsets: ['latin'], display: 'swap' });
 
-export async function generateMetadata(): Promise<Metadata> {
-    const { data } = await apiRequest(FrontPageMetadataDocument);
-
-    const meta = data.browse?.landingPage?.hits?.[0]?.meta;
-    const title = meta?.title ?? '';
-    const description = meta?.description[0].textContent;
-    const image = meta?.image?.[0];
-    const baseUrl  = new URL(process.env.NEXT_PUBLIC_CANONICAL_URL ?? 'http://localhost:3000')
-
-    return {
-        title: {
-            default: title,
-            template: '%s | Furnitut',
-            absolute: `${title} | Furnitut`,
-        },
-        alternates: {
-            canonical: baseUrl
-        },
-        metadataBase: baseUrl,
-        description,
-        creator: 'Crystallize Team',
-        openGraph: {
-            title: `${title} | Furnitut`,
-            description,
-            images: [
-                {
-                    url: image?.url ?? '',
-                    alt: image?.altText ?? '',
-                    height: image?.height ?? 0,
-                    width: image?.width ?? 0,
-                },
-            ],
-        },
-    };
-}
-
 type LayoutProps = { children: React.ReactNode };
 
 export default async function Layout({ children }: LayoutProps) {
