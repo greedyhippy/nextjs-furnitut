33c9e35c7859d7c5687ca18e829f0d946ad7d2b5
didrikhegna <didrik@snowball.digital>
2025-05-22 14:26:40 +0200

    removed test in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverComponentsExternalPackages: ['@react-pdf/renderer'],
     experimental: {
         ppr: 'incremental',
     },
