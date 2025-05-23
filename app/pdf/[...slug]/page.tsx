e53ec1966f04f01b6cb489a0ef0fef8dfa728bda
didrikhegna <didrik@snowball.digital>
2025-05-23 12:01:38 +0200

    reworked pdf preview

--- app/pdf/[...slug]/page.tsx
+++ app/pdf/[...slug]/page.tsx
@@ -1,27 +1,14 @@
-import { PDFViewer, ProductPDF } from '@/components/pdf';
-import { fetchProductDataForPDF } from '@/components/pdf/utils';
 type PDFProps = {
     params: Promise<{ slug: string[] }>;
 };
 export default async function PDFPage({ params }: PDFProps) {
     const { slug } = await params;
     slug.pop();
-    const path = `/${slug.join('/')}`;
+    const pdfUrl = `/api/pdf?path=${encodeURIComponent(slug)}`;
 
-    const productData = await fetchProductDataForPDF(path);
-    const { variants } = productData;
-    if (!variants || !variants.length) {
-        return (
-            <div className="h-screen w-full flex justify-center items-center font-bold">
-                No support for this shape type
-            </div>
-        );
-    }
     return (
-        <div className="min-h-screen w-full">
-            <PDFViewer className="min-h-screen w-full">
-                <ProductPDF product={productData} />
-            </PDFViewer>
+        <div style={{ height: '100vh' }}>
+            <iframe src={pdfUrl} style={{ width: '100%', height: '100%', border: 'none' }} />
         </div>
     );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ['@react-pdf/renderer'],
     typescript: {
         ignoreBuildErrors: true,
     },
     experimental: {
         ppr: 'incremental',
-        esmExternals: 'loose',
     },
 };
 
